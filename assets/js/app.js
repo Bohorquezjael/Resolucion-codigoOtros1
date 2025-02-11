9b68b99d1e6b0ea36e7477b5db8bfeee8da2ac59
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-11 17:56:44 -0600

    cambios para que funcione el css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
 
-// Corrección en la selección de elementos estaba mal seleccionado por id
-const $n = document.querySelector('#name');  
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// Corrección en la selección de elementos por clase y variables mas descriptivas
+const name = document.getElementsByClassName(".name");  
+const blogName = document.getElementsByClassName(".blog");
+const location = document.getElementsByClassName(".location");
 
 //hago la funcion asincrona para que pueda usar el await
 async function displayUser(username) {
   //manejo una posible excepcion a causa del fetch
   try {
-    $n.textContent = 'Cargando...';
+    name.textContent = 'Cargando...';
     const response = await fetch(`${usersEndpoint}/${username}`);
     //si la respuesta de la peticion no es un ok(HTTP:200) lanzo excecion y la manejo con el catch
     if (!response.ok) {
@@ -21,9 +21,9 @@ async function displayUser(username) {
     //imprimo los objetos del json resuelto
     console.log(data);
     //mediante corto circuito extraaigo los valores del objeto o si no los tiene les asigno uno por default
-    $n.textContent = data.name || 'Sin nombre';
-    $b.textContent = data.blog || 'Sin blog';
-    $l.textContent = data.location || 'Ubicación desconocida';
+    name.innerText = data.name || 'Sin nombre';
+    blogName.innerText = data.blog || 'Sin blog';
+    location.innerText = data.location || 'Ubicación desconocida';
   } catch (err) {
     handleError(err);
   }
@@ -32,7 +32,7 @@ async function displayUser(username) {
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  $n.textContent = `Algo salió mal: ${err.message}`;
+  name.textContent = `Algo salió mal: ${err.message}`;
 }
 
 //por ultimo slolo llamo a la funcion para que colocque la informacion en los  elementos html
